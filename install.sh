14d1970f8102ef65588a38e809f048073b9ab11c
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-03-03 11:18:06 +0100

    feat: Add pretty_print to install script
    Also print an info if 42free alias is already present.

--- install.sh
+++ install.sh
@@ -7,10 +7,16 @@ script_url="https://raw.githubusercontent.com/itislu/42free/main/42free.sh"
 dest_dir="$HOME/.scripts"
 dest_file="42free.sh"
 
+# Automatically detects the size of the terminal window and preserves word boundaries at the edges
+pretty_print()
+{
+    printf "%b" "$1" | fmt -sw $(tput cols)
+}
+
 # Download the script
 mkdir -p "$dest_dir"
 curl -Lo "$dest_dir/$dest_file" "$script_url"
-echo -e "'$dest_file' downloaded into '$dest_dir'."
+pretty_print "'$dest_file' downloaded into '$dest_dir'."
 
 # Make the script executable
 chmod +x "$dest_dir/$dest_file"
@@ -23,20 +29,22 @@ elif [[ "$SHELL" == *"zsh"* ]]; then
 elif [[ "$SHELL" == *"fish"* ]]; then
     RC_FILE="$HOME/.config/fish/config.fish"
 else
-    echo -e "\e[1;31mUnsupported shell. Please set an alias for your shell manually.\e[0m"
+    pretty_print "\e[1;93mUnsupported shell. Please set an alias for your shell manually.\e[0m"
     exit 1
 fi
 
 # Add an alias to the user's shell RC file if it doesn't exist
 if ! grep "42free=" "$RC_FILE" &> /dev/null; then
-    echo -e "\e[33m42free alias not present.\e[0m"
-    echo -e "\e[33mAdding alias in file: $RC_FILE\e[0m"
+    pretty_print "\e[33m42free alias not present.\e[0m"
+    pretty_print "\e[33mAdding 42free alias in file '$RC_FILE'.\e[0m"
     echo -e "\nalias 42free='bash $dest_dir/$dest_file'\n" >> "$RC_FILE"
+else
+    pretty_print "\e[33m42free alias already present.\e[0m"
 fi
 
-echo -e "\e[1;32mInstallation completed.\e[0m"
-echo -e "You can now use the 42free command."
-echo -e "For help, run '\e[1m42free -h\e[0m'."
+pretty_print "\e[1;32mInstallation completed.\e[0m"
+pretty_print "You can now use the 42free command."
+pretty_print "For help, run '\e[1m42free -h\e[0m'."
 
 # Start the default shell to make the alias available immediately
 exec $SHELL
