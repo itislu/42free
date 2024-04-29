31735851750d07fe6684dd0ff11073ccb30cfdb4
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-04-29 14:10:27 +0200

    fix: Remove Firefox from default arguments
    Firefox could not be opened anymore.
    For some reason Firefox does not follow symbolic links.

--- 42free.sh
+++ 42free.sh
@@ -12,7 +12,6 @@ default_args=(\
 "$HOME/.var/app/com.brave.Browser/cache" \
 "$HOME/.var/app/com.google.Chrome/cache" \
 "$HOME/.var/app/com.opera.Opera/cache" \
-"$HOME/.var/app/org.mozilla.firefox/cache" \
 )
 
 # Standard variables
