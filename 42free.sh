a66ed39f2dabfb1d622c7ddb1df7462a4a568f50
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-05-01 00:21:03 +0200

    fix: Remove too many newlines at end of help message

--- 42free.sh
+++ 42free.sh
@@ -137,7 +137,7 @@ $delim_small
 
 To contribute, report bugs or share improvement ideas, visit
 ${underlined}${bright_blue}https://github.com/itislu/42free${reset}.
-\n"
+"
 
 msg_version="\
 ${bold}42free $current_version${reset}
